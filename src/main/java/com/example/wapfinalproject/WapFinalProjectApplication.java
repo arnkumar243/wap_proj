34f85b89bce30bcd7b3837115d325c94f459bb1d
Arunkumar <arnkumar2403@gmail.com>
2023-03-10 12:26:56 -0800

    first commit

---
+++ src/main/java/com/example/wapfinalproject/WapFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.wapfinalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WapFinalProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WapFinalProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wapfinalproject/WapFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wapfinalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WapFinalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
